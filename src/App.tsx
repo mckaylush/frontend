3b9fb3b95e9fcfea0dadb5faba1940055222a1f3
mckaylush <130932201+mckaylush@users.noreply.github.com>
2024-03-12 00:06:52 -0600

    part one mar 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import movieData from './MovieData.json';
+
+const MovieDataSample = movieData.MovieData;
+
+function MovieList() {
+  const [listoMovies, setListOfMovies] = useState(MovieDataSample);
+
+  const addMovie = () => {
+    setListOfMovies([
+      ...MovieDataSample,
+      {
+        Category: 'Action/Adventure',
+        Title: 'Batman Returns',
+        Year: 1992,
+        Director: 'Tim Burton',
+        Rating: 'PG-13',
+      },
+    ]);
+  };
+
+  return (
+    <>
+      <h3>Movie Collection List</h3>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Year</th>
+            <th>Director</th>
+            <th>Rating</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listoMovies.map((m) => (
+            <tr>
+              <td>{m.Title}</td>
+              <td>{m.Year}</td>
+              <td>{m.Director}</td>
+              <td>{m.Rating}</td>
+              <td>{m.Category}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <button onClick={addMovie}>Add Movie</button>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieList />
     </div>
   );
 }
